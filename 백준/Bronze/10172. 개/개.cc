3ca9277b7f19f3ab76a36c31c5fed2ae960b5f0c
JANGYERIM <139464483+JANGYERIM@users.noreply.github.com>
2025-05-12 23:33:48 +0900

    [Bronze V] Title: 개, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define fastio cin.tie(0)->sync_with_stdio(0)
+#define endl '\n'
+using namespace std;
+
+int main() {
+    fastio;
+    cout << "|\\_/|" << endl;
+    cout << "|q p|   /}" << endl;
+    cout << "( 0 )\"\"\"\\" << endl;
+    cout << "|\"^\"`    |" << endl;
+    cout << "||_/=\\\\__|" << endl;
+
+    return 0;
+}
