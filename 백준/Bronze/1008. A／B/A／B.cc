76721419a6b9b3c3f7f88355dc73648cfc107b6e
JANGYERIM <139464483+JANGYERIM@users.noreply.github.com>
2025-05-12 22:29:55 +0900

    [Bronze V] Title: A/B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    double a, b;
+    cin >> a >> b;
+    cout.precision(12);
+    cout<< fixed;
+    cout << a / b << endl;
+    
+    return 0;
+}
