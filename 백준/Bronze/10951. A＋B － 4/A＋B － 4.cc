724d7189a3fda877e651ee1ff850b9661b59d34c
JANGYERIM <139464483+JANGYERIM@users.noreply.github.com>
2025-05-13 14:01:12 +0900

    [Bronze V] Title: A+B - 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+using namespace std;
+
+int main() {
+    int a, b;
+    while(cin >> a >> b){
+        cout << a+b << '\n';
+    }
+
+    return 0;
+
+}
