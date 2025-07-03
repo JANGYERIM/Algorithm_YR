fe03db1baf8936421284c9c86438665ff3d960fd
JANGYERIM <139464483+JANGYERIM@users.noreply.github.com>
2025-07-03 23:08:40 +0900

    [Gold IV] Title: 불, Time: 152 ms, Memory: 14684 KB -BaekjoonHub

--- 백준/Gold/5427. 불/불.cc
+++ 백준/Gold/5427. 불/불.cc
@@ -1,3 +1,5 @@
+
+
 #include <bits/stdc++.h>
 #define X first
 #define Y second
@@ -17,7 +19,7 @@ bool outOfBound(int x, int y){
     return x<0||x>=r||y<0||y>=c;
 }
 bool isEscape_F(int x, int y) {
-    return x==0||x==r-1 || y==0|| y ==c-1; 
+    return x==-1||x==r || y==-1|| y ==c; 
 }
 
 int main() {
@@ -42,6 +44,13 @@ int main() {
                     FQ.push({i,j});
                 }
                 if(input == '@'){
+                    if(isEscape_F(i,j)){
+                        if(!isEscape){
+                            cout << 1 << endl;
+                            isEscape = true;
+                            break; 
+                        }
+                    }
                     escape[i][j] = 0;
                     SQ.push({i,j});
                 }
@@ -64,20 +73,19 @@ int main() {
         int escapeTime = r*c;
         while(!SQ.empty()&&!isEscape){
             auto cur = SQ.front(); SQ.pop();
-            if(isEscape_F(cur.X,cur.Y) && escape[cur.X][cur.Y] <=escapeTime){
-                        isEscape = true;
-                        escapeTime =escape[cur.X][cur.Y] + 1 ;
-                        cout << escapeTime<< endl;
-                    }
 
             for(int dir=0; dir < 4; dir ++){
                 int nx = cur.X + dx[dir];
                 int ny = cur.Y + dy[dir];
-
-                if(outOfBound(nx,ny)) continue;
+                if(isEscape_F(nx,ny)){
+                    isEscape = true;
+                    cout << escape[cur.X][cur.Y]+1<< endl;
+                    break;
+                }
                 if(escape[nx][ny] != -1 || board[nx][ny] == '#') continue;
                 escape[nx][ny] = escape[cur.X][cur.Y] +1;
                 if(escape[nx][ny] < vis[nx][ny] || vis[nx][ny] == -1){
+                    
                     SQ.push({nx,ny});
                 }
             }
