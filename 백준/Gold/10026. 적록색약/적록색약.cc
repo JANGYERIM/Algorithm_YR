c40be52d56ca70d999c42f6c097458313d476746
JANGYERIM <139464483+JANGYERIM@users.noreply.github.com>
2025-07-03 19:33:45 +0900

    [Gold V] Title: 적록색약, Time: 0 ms, Memory: 2136 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.cc
+++ 백준/Gold/10026. 적록색약/적록색약.cc
@@ -16,6 +16,42 @@ bool outOfBound(int x, int y){
     return x<0||x>=m||y<0||y>=n;
 }
 
+void bfs(int i, int j) {
+
+    vis[i][j] = 1;
+    queue<pair<int,int>> Q;
+
+    Q.push({i,j});
+
+    while(!Q.empty()){
+        auto cur = Q.front(); Q.pop();
+        
+        for(int dir=0; dir < 4; dir ++){
+            int nx = cur.X + dx[dir];
+            int ny = cur.Y + dy[dir];
+
+            if(outOfBound(nx,ny)) continue;
+            if(vis[nx][ny] != -1 || board[i][j] != board[nx][ny]) continue;
+            vis[nx][ny] = 1;
+            Q.push({nx,ny});
+        }
+    }
+    
+}
+
+int area(){
+    int cnt = 0;
+    for(int i =0; i < n; i ++){
+        for(int j=0; j< n; j ++){
+            if(vis[i][j] ==-1){
+                cnt ++;
+                bfs(i,j);
+            }
+        }
+    }
+    return cnt;
+}
+
 
 int main() {
     cin >> n;
@@ -31,102 +67,21 @@ int main() {
             j++;
         }
     }
-    queue<pair<int,int>> RQ;
-    queue<pair<int,int>> BQ;
-    queue<pair<int,int>> GQ;
-    queue<pair<int,int>> R_GQ;
-    int R =0; int B = 0; int G =0; int R_G =0;
-    for(int i=0; i < m; i ++){
-        for (int j=0; j< n; j++){
-          if(vis[i][j] ==-1 && board[i][j] =='R' ){
-              vis[i][j] = 1;
-              RQ.push({i,j});
-              R++;
 
-              while(!RQ.empty()){
-                  auto cur = RQ.front(); RQ.pop();
-                  for(int dir =0 ; dir < 4; dir ++){
-                      int nx = cur.X + dx[dir];
-                      int ny = cur.Y + dy[dir];
+    int rgb_cnt = area();
 
-                      if(outOfBound(nx,ny)) continue;
-                      if(vis[nx][ny] != -1 || board[nx][ny] != 'R') continue;
-                      vis[nx][ny] = 1;
-                      RQ.push({nx,ny});
-                      
-                  }
-              }
-          }
-            if(vis[i][j] ==-1 && board[i][j] =='G' ){
-              vis[i][j] = 1;
-              GQ.push({i,j});
-              G++;
-
-              while(!GQ.empty()){
-                  auto cur = GQ.front(); GQ.pop();
-                  for(int dir =0 ; dir < 4; dir ++){
-                      int nx = cur.X + dx[dir];
-                      int ny = cur.Y + dy[dir];
-
-                      if(outOfBound(nx,ny)) continue;
-                      if(vis[nx][ny] != -1 || board[nx][ny] != 'G') continue;
-                      vis[nx][ny] = 1;
-                      GQ.push({nx,ny});
-                      
-                  }
-              }
-          }
-
-            if(vis[i][j] ==-1 && board[i][j] =='B' ){
-              vis[i][j] = 1;
-              BQ.push({i,j});
-              B++;
-
-              while(!BQ.empty()){
-                  auto cur = BQ.front(); BQ.pop();
-                  for(int dir =0 ; dir < 4; dir ++){
-                      int nx = cur.X + dx[dir];
-                      int ny = cur.Y + dy[dir];
-
-                      if(outOfBound(nx,ny)) continue;
-                      if(vis[nx][ny] != -1 || board[nx][ny] != 'B') continue;
-                      vis[nx][ny] = 1;
-                      BQ.push({nx,ny});
-                      
-                  }
-              }
-          }
-        }
-    }
 
     memset(vis, -1, sizeof(vis));
     for(int i=0; i < m; i ++){
         for (int j=0; j< n; j++){
-            if(vis[i][j] ==-1 && (board[i][j] =='R'|| board[i][j] =='G' )){
-              vis[i][j] = 1;
-              R_GQ.push({i,j});
-              R_G++;
-
-              while(!R_GQ.empty()){
-                  auto cur = R_GQ.front(); R_GQ.pop();
-                  for(int dir =0 ; dir < 4; dir ++){
-                      int nx = cur.X + dx[dir];
-                      int ny = cur.Y + dy[dir];
-
-                      if(outOfBound(nx,ny)) continue;
-                      if(vis[nx][ny] != -1 || board[nx][ny] == 'B') continue;
-                      vis[nx][ny] = 1;
-                      R_GQ.push({nx,ny});
-                      
+            if(board[i][j] =='G' ){
+              board[i][j] = 'R';
+                                    
                   }
               }
           }
-        }
-    }
+    int rb_cnt = area();
     
-    cout << R+G+B << ' ' << R_G + B << endl;
-
-    
-    
+    cout << rgb_cnt << ' ' << rb_cnt << endl;
     return 0;
 }
