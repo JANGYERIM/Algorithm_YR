a58e4a52428e12ad546d2e9ecbe86bdb1d28429f
JANGYERIM <139464483+JANGYERIM@users.noreply.github.com>
2025-08-04 18:05:54 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 0 ms, Memory: 2936 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.cc
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.cc
@@ -4,13 +4,25 @@
 using namespace std;
 #define endl '\n'
 
+deque<int> Q;
 int n, k;
 int board[200002];
-deque<int> Q;
+
+int moveFunction(int dir, int cur) {
+    if (dir == 0) return cur * 2;
+    else if (dir == 1) return cur + 1;
+    else return cur - 1;
+}
 
 int main() {
     cin >> n >> k;
     memset(board, -1, sizeof(board));
+
+    if (n == k) {
+        cout << 0 << endl;
+        return 0;
+    }
+
     board[n] = 0;
     Q.push_front(n);
 
@@ -18,25 +30,16 @@ int main() {
         int cur = Q.front(); Q.pop_front();
 
         for (int dir = 0; dir < 3; dir++) {
-            int nx;
-            int cost;
-            if (dir == 0) {
-                nx = cur * 2;
-                cost = 0;
-            } else if (dir == 1) {
-                nx = cur + 1;
-                cost = 1;
-            } else {
-                nx = cur - 1;
-                cost = 1;
-            }
-
+            int nx = moveFunction(dir, cur);
             if (nx < 0 || nx >= 200002) continue;
 
-            if (board[nx] == -1 || board[nx] > board[cur] + cost) {
-                board[nx] = board[cur] + cost;
-                if (cost == 0) Q.push_front(nx);
-                else Q.push_back(nx);
+            if (dir == 0 && (board[nx] == -1 || board[nx] > board[cur])) {
+                board[nx] = board[cur];
+                Q.push_front(nx);
+            }
+            else if (dir > 0 && (board[nx] == -1 || board[nx] > board[cur] + 1)) {
+                board[nx] = board[cur] + 1;
+                Q.push_back(nx);
             }
         }
     }
